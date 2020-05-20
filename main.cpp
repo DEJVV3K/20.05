adbbcc6a5e764993a3b2554e25446187df8b45ca
DEJVV3K <62350376+DEJVV3K@users.noreply.github.com>
2020-05-20 10:39:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int wej, tot = 0;
+	while(scanf("%d", &wej) != EOF)
+		printf("%d\n", tot += wej);
+		
+int main ()
+{
+	ifstream wejscie;
+	ofstream wyjscie;
+	
+	wejscie.open(wejscie.txt);
+	wyjscie.open(wyjscie.json);
+	
+	int licznik = 0;
+	
+	if (wejscie.good())
+	{
+		while(!wejscie.eof())
+		{
+			for(int i = 0; i < 1000; i++)
+			wejscie >> t[i];
+		}
+		for(int i= 0; i >= licznik; i++)
+		{
+			if(i == licznik)
+			{
+				if()
+			}
+		}
+	}
+	
+	wejscie.close();
+	wyjscie.close();
+	
+	return 0;
+}
